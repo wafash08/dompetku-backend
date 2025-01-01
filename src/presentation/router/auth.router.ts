dbd0e28c63ad2c1e6697e87186c84837b12c602c
wafash08 <saefulhaqwafa@gmail.com>
2025-01-01 17:19:44 +0700

    login route

--- src/presentation/router/auth.router.ts
+++ src/presentation/router/auth.router.ts
@@ -33,4 +33,31 @@ export const authRouter = new Elysia()
 				password: t.String({ minLength: 8 }),
 			}),
 		},
+	)
+	.post(
+		"/login",
+		async ({ body, set }) => {
+			try {
+				const session = await authServices.loginUser({
+					email: body.email,
+					password: body.password,
+				});
+
+				return { session };
+			} catch (error) {
+				set.status = 500;
+
+				if (error instanceof Error) {
+					throw new Error(error.message);
+				}
+
+				throw new Error("Something went wrong");
+			}
+		},
+		{
+			body: t.Object({
+				email: t.String({ format: "email" }),
+				password: t.String({ minLength: 8 }),
+			}),
+		},
 	);
