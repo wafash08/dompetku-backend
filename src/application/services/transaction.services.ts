0bcf9455b6b475d69d075a167b5efe7baf2a0533
wafash08 <saefulhaqwafa@gmail.com>
2025-01-01 16:49:01 +0700

    note services

---
+++ src/application/services/transaction.services.ts
@@ -0,0 +1,44 @@
+import type {
+	CreateTransaction,
+	UpdateTransaction,
+} from "../../infrastructure/entities/interface";
+import type { TransactionRepository } from "../../infrastructure/repositories/transaction.repository";
+
+export class TransactionServices {
+	private _transactionRepository: TransactionRepository;
+
+	constructor(transactionRepository: TransactionRepository) {
+		this._transactionRepository = transactionRepository;
+	}
+
+	async getAll(userId: string) {
+		const transactions = await this._transactionRepository.getAll(userId);
+
+		return transactions;
+	}
+
+	async getOne(transactionId: string) {
+		const transaction = await this._transactionRepository.getOne(transactionId);
+
+		return transaction;
+	}
+
+	async create(data: CreateTransaction) {
+		const newTransaction = await this._transactionRepository.create(data);
+
+		return newTransaction;
+	}
+
+	async update(transactionId: string, data: UpdateTransaction) {
+		const updatedTransaction = this._transactionRepository.update(
+			transactionId,
+			data,
+		);
+
+		return updatedTransaction;
+	}
+
+	async delete(transactionId: string) {
+		await this._transactionRepository.delete(transactionId);
+	}
+}
